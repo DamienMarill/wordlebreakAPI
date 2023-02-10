f7bb26f59d86387a29737f70994896421e84fb8f
Damien Marill <dev@marill.dev>
2023-02-10 23:07:27 +0100

    Import mots bdd

--- database/migrations/2023_02_10_213542_create_words_table.php
+++ database/migrations/2023_02_10_213542_create_words_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -13,15 +14,15 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create('words', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->string('word');
+            $table->string('word_clean')->nullable();
+            $table->timestamps();
         });
+
+        Artisan::call('words:import');
+        Artisan::call('words:clean');
     }
 
     /**
@@ -31,6 +32,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('words');
     }
 };
